44f47a510ba3e1cbbcd083de20f4b82f83c7718e
Venkat <haichary@yahoo.co.in>
2019-11-05 07:18:12 -0500

    adding webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+const path = require('path');
+var webpack = require('webpack');
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          }
+        ]
+      }
+    ]
+  },
+};
